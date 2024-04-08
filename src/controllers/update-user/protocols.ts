e4de8a1c780f5e595961e1c9bad0ffa3a3b1885f
teomuto <teomutoeb@gmail.com>
2024-04-08 19:08:09 -0300

    feat: add MongoUpdateUserRepository

---
+++ src/controllers/update-user/protocols.ts
@@ -0,0 +1,19 @@
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+
+export interface UpdateUserParams {
+  firstName?: string;
+  lastName?: string;
+  password?: string;
+}
+
+export interface IUpdateUserRepository {
+  updateUser(id: string, params: UpdateUserParams): Promise<User>;
+}
+
+export interface IUpdateUserController {
+  handle(
+    id: string,
+    httpRequest: HttpRequest<UpdateUserParams>
+  ): Promise<HttpResponse<User>>;
+}
