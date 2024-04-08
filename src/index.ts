95ce4cc3ae5ceb26ea8a42c0250dff9a5db0b4ae
teomuto <teomutoeb@gmail.com>
2024-04-08 17:19:32 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("aoba")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/users", (req, res) => {
+  res.send("aoba");
+});
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
